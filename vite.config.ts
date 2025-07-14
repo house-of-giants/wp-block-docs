5d4c1705c7ff11776058dd423cd9cda8cbba9d51
Dominic Magnifico <dominicmagnifico@gmail.com>
2025-07-14 10:18:40 -0600

    Update pnpm-lock.yaml to add and remove dependencies; clean up vite.config.ts by removing unused alias

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
-      "@shared": path.resolve(__dirname, "./shared"),
     },
   },
 }));
